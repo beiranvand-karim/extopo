125b496d8f479dc3ff0376a70720ec1f1c35b0d3
karim <beiranvand.karim@gmail.com>
2018-09-22 10:37:28 +0330

    initial commit

---
+++ back-end/mongo.js
@@ -0,0 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URL = "mongodb://localhost:27017/polyglot_ninja";
+
+module.exports = function (app) {
+    MongoClient.connect(MONGO_URL, { useNewUrlParser: true })
+        .then((client) => {
+            const db = client.db('polyglot_ninja');
+            app.people = db.collection('people');
+            console.log('Database connection established')
+        })
+        .catch((err) => console.error(err))
+};
